c1ab62b80af95e2f136834ec184842245412f16e
RyanStewart <ryan92130@gmail.com>
2018-08-12 02:02:40 -0400

    Dynamically populate the build bar.

--- Assets/Scripts/Managers/BuildManager.cs
+++ Assets/Scripts/Managers/BuildManager.cs
@@ -9,11 +9,13 @@ public class BuildManager : MonoBehaviour {
     public BuildingBlueprint instantiatedBlueprint;
 
     public RectTransform menuParent;
-    public Button menuItemPrefab;
+    public BuildMenuItem menuItemPrefab;
+
+    public List<BuildingBase> buildings;
 
 	// Use this for initialization
 	protected void Start () {
-        
+        CreateMenuItems();
 	}
 	
 	// Update is called once per frame
@@ -29,5 +31,25 @@ public class BuildManager : MonoBehaviour {
         }
         instantiatedBlueprint = Instantiate(blueprintPrefab);
         instantiatedBlueprint.SetupBlueprint(building);
+    }
+
+    // creates a new menu item
+    public BuildMenuItem CreateMenuItem (BuildingBase buildingBase)
+    {
+        BuildMenuItem item = Instantiate(menuItemPrefab, menuParent);
+        item.SetupItem(buildingBase, SelectBuild);
+        return item;
+    }
+
+    public void CreateMenuItems ()
+    {
+        int i = 0;
+        foreach (BuildingBase buildingBase in buildings)
+        {
+            RectTransform rt = CreateMenuItem(buildingBase).GetComponent<RectTransform>();
+            rt.anchoredPosition = new Vector2(0, -90 - (i * 170));
+            i++;
+        }
+        menuParent.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 20 + (i * 170));
     }
 }
---
+++ Assets/Scripts/UI/BuildMenuItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildMenuItem : MonoBehaviour {
+
+    public Text text;
+    public Image image;
+    public Button button;
+    public delegate void Action(BuildingBase buildingBase);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetupItem(BuildingBase buildingBase, Action action)
+    {
+        this.text.text = buildingBase.displayName;
+        this.image.sprite = buildingBase.GetComponent<SpriteRenderer>().sprite;
+        this.button.onClick.AddListener(delegate { action(buildingBase); });
+    }
+}
