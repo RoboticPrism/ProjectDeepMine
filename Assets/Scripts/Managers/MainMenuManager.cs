0e3239e289daa28de0379955b292b8d59d00f4b2
RyanStewart <ryan92130@gmail.com>
2018-10-08 23:05:27 -0400

    Added basic main menu.  Staged menu and level select into build settings.

---
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartAction()
+    {
+        SceneManager.LoadSceneAsync("LevelSelect");
+    }
+
+    public void ExitAction()
+    {
+        Application.Quit();
+    }
+}
